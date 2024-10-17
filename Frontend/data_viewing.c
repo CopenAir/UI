696253328d59e4c0c37340e5c433a6d041d8b8c8
AserDevel <asbjorn.biering@gmail.com>
2024-10-17 17:30:51 +0200

    small refactoring

--- Frontend/data_viewing.c
+++ Frontend/data_viewing.c
@@ -9,15 +9,14 @@ int max(int a, int b) {
 // Draw a graph of the given float data
 // n is length of data
 void draw_graph(float data[], int n) {
-    // copy and convert the data to integers for display purposes
+    // copy and convert the data to integers for display purposes and get 
+    // the maximum value in data_as_int to know how tall the graph should be
     int data_as_int[n];
-    for (int i = 0; i < n; i++)
-        data_as_int[i] = (int)(data[i] * 10);
-    
-    // get the maximum value in data_as_int to know how tall the graph should be
     int m;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        data_as_int[i] = (int)(data[i] * 10);
         m = max(m, data_as_int[i]);
+    }
 
     // Draw graph from top left to bottom right
     for (int i = m; i > 0; i--) {
