dbc7383956123f4c6f005753527c45d3fac84496
AserDevel <asbjorn.biering@gmail.com>
2024-10-18 13:48:27 +0200

    Fixed a few things in the frontend

--- Frontend/data_display.c
+++ Frontend/data_display.c
@@ -12,28 +12,30 @@ int max(int a, int b) {
 // n is the number of elements in the array
 // threshold is the value where the data starts to get red
 void draw_graph(float data[], int n, float threshold) {
-    // copy and convert the data to integers so it can be used in the for loop,
+    // copy and convert the data to integers so it can be used in the for loop.
+    // We cut off some precision to fit the graph in the terminal,
     // and get the maximum value to know how tall the graph should be
     int data_as_int[n];
     int mx = 0;
     for (int i = 0; i < n; i++) {
-        data_as_int[i] = (int)(data[i] * 10);
+        data_as_int[i] = (int)(data[i] * 2); 
         mx = max(mx, data_as_int[i]);
     }
+    mx += 10 - (mx % 10); // round up for a more pretty graph
 
     // Draw graph from top left to bottom right
     for (int i = mx; i > 0; i--) {
-        if (i > (threshold * 10)) {
+        if (i > (threshold * 2)) 
             printf("\033[0;31m"); // Switch print color to red
-            printf("%4.1f ║", ((float) i) / 10);
-        }
-        else {
+        else 
             printf("\033[0;32m"); // Switch print color to green
-            printf("%4.1f ║", ((float) i) / 10);
-        }
+        if (i % 10 == 0) // display x-axis value for every 10th line
+            printf("%4d ║", i / 2);
+        else
+            printf("     ║");
         for (int j = 0; j < n; j++) {
             if (data_as_int[j] >= i)
-                printf(" ###");
+                printf(" ███");
             else
                 printf("    ");
         }
@@ -51,80 +53,66 @@ void draw_graph(float data[], int n, float threshold) {
     printf("\n"); 
 }
 
-void display(float data[])
+// Draw the data in allignment
+void draw_data(float data[])
 {
-    printf("--------------------------------------------\n");
-    char pm25[] = "PM2.5:";
-    char pm10[] = "PM10: ";
-    char no2[] = "NO2:  ";
-    char temp[] = "Temp: ";
-    printf("%s %5.1f\n", pm25, data[0]);
-    printf("%s %5.1f\n", pm10, data[1]);
-    printf("%s %5.1f\n", no2, data[2]);
-    printf("%s %5.1f\n", temp, data[3]);   
-    printf("--------------------------------------------\n");
+    printf("---------------------------------------------\n\n");
+    printf("Substance   μg/m3\n\n");
+    printf("[%d]PM2.5: %5.1f\n", 1, data[0]);
+    printf("[%d]PM10:  %5.1f\n", 2, data[1]);
+    printf("[%d]NO2:   %5.1f\n", 3, data[2]);
+    printf("[%d]Temp:  %5.1f\n", 4, data[3]);   
+    printf("\n---------------------------------------------\n");
 }
 
-void average(float** data, int n, float* avg)
-{
-    float pm25_total = 0, pm10_total = 0, no2_total = 0, temp_total = 0;
+// Calculates the averages of the given data set
+void average(float** data, int n, float* avg) {
+    float pm25_sum = 0, pm10_sum = 0, no2_sum = 0, temp_sum = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        pm25_total += data[i][1];
-        pm10_total += data[i][2];
-        no2_total += data[i][3];
-        temp_total += data[i][4];
+    for (int i = 0; i < n; i++) {
+        pm25_sum += data[i][1];
+        pm10_sum += data[i][2];
+        no2_sum += data[i][3];
+        temp_sum += data[i][4];
     }
 
-    avg[0] = pm25_total / n;
-    avg[1] = pm10_total / n;
-    avg[2] = no2_total / n;
-    avg[3] = temp_total / n;
-    
+    avg[0] = pm25_sum / n;
+    avg[1] = pm10_sum / n;
+    avg[2] = no2_sum / n;
+    avg[3] = temp_sum / n; 
 }
 
 // Displays the data for the given hour
-void hour_display(char* date, int hour, char road[]) {
-    float* data;
-    //data = request_data(year, month, day, hour);
+void hour_display(char* road, char* date, int hour) {
+    printf("\033[H\033[J");  // ANSI escape code to clear the screen
+    float data[4] = {1, 2, 3, 4};
 
-    printf("%s\n", road);
-    printf("Hourview         ");
-    printf("%s %10d:00\n", date, hour);
+    printf("%-20s%-20s%d:00\n", road, date, hour);
 
-
-    float a[] = {1, 2, 3, 4};
-    display(a);
+    draw_data(data);
 }
 
 // Displays data for the given day
-void day_display(char* date, char road[]) {
-    float* a[5];
-    float b[5] = {1 ,2 ,3 ,4 ,5};
-    float c[5] = {5, 4, 3, 2, 1}; 
+void day_display(char* road, char* date) {
+    printf("\033[H\033[J");  // ANSI escape code to clear the screen
+    float* data[4];
+    float a[5] = {1 ,2 ,3 ,4 ,5};
+    float b[5] = {5, 4, 3, 2, 1}; 
 
-    a[0] = b;
-    a[1] = c;
-    a[2] = c;
-    a[3] = b;
-    a[4] = b;
+    data[0] = a;
+    data[1] = a;
+    data[2] = b;
+    data[3] = b;
 
     float avg[4];
+    average(data, 4, avg);
 
-    average(a, 5, avg);
+    printf("%-35s%s\n", road, date);
 
-    printf("%s\n", road);
-    printf("Day average                       %s \n", date);
-
-    display(avg);
-
+    draw_data(avg);
 }
 
 // Displays data for the given month
-void month_displayi(char* date) {
-    float* data;
-    //data = request_data(year, month);
+void month_display(char* road, char* date) {
+    // todo
 }
-
-
--- Frontend/data_display_test.c
+++ Frontend/data_display_test.c
@@ -7,30 +7,25 @@ void draw_graph_test() {
     // Perform 10 test with random data
     for (int test = 0; test < 10; test++) {
         for (int i = 0; i < 24; i++)
-            data[i] = ((float)(rand() % 20)) / 10; // random float between 0 and 2.0
+            data[i] = ((float)(rand() % 200)) / 10; // random float between 0 and 20.0
         draw_graph(data, 24, data[0]);
     }
-    // testing 2 digit data, negative data and zero
-    float new_data[3];
+    // Testing negative data and zero
+    float new_data[2];
     new_data[0] = -1.0;
     new_data[1] = 0.0;
-    new_data[2] = 10.11;
-    draw_graph(new_data, 3, 5.0);
+    draw_graph(new_data, 2, 0);
     // Empty list
     float* empty_data;
-    draw_graph(empty_data, 0, 0.1);
+    draw_graph(empty_data, 0, 0);
 }
 
 void hour_display_test() {
-    char a[] = "A.C. Meyers";
-    char b[] = "2024-10-18";
-    hour_display(b, 2, a);
+    hour_display("A.C. Meyers", "2024-10-18", 10);
 }
 
 void day_display_test() {
-    char a[] = "A.C Meyer";
-    char b[] = "2024-10-18";
-    day_display(b, a);
+    day_display("A.C. Meyers", "2024-10-18");
 }
 
 void month_display_test() {
@@ -39,7 +34,7 @@ void month_display_test() {
 
 // Call tests in main function
 void main() {
-    //draw_graph_test();
+    draw_graph_test();
     //hour_display_test();
-    day_display_test();
+    //day_display_test();
 }
