0f7fa8c2f29c61c5f3d797ad05b02c5a6ed18796
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2024-11-21 11:05:27 +0100

    cleaned up csv.h and data_display.h

--- Final/lib/Frontend/data_display.h
+++ Final/lib/Frontend/data_display.h
@@ -7,10 +7,5 @@
 void draw_graph(int size, float *data, float max_val, float threshold);
 void print_rating(float data, int threshold);
 void print_data(float data[]);
-void print_data(float data[]);
-void average(float** data, int n, float* avg);
-void hour_display(char* road, char* date, int hour);
-void day_display(char* road, char* date);
-void month_display(char* road, char* date);
 
 #endif
--- Final/lib/csv_backend/csv.h
+++ Final/lib/csv_backend/csv.h
@@ -10,6 +10,5 @@
 int load_data(char *filename, float data_matrix[][MAX_ROWS]);
 int get_data_for_date(char *filename, float data_array[], time_t date);
 int get_data_for_timespan(const char *filename, float data_matrix[MAX_COLUMNS][MAX_ROWS], struct timespan timespan);
-int is_midday(time_t unixtime);
 
 #endif
